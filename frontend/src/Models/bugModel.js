6ff720e378bbfbcb01bd19f358fcbc51ec58260c
Tyler <Tyl3r.s@outlook.com>
2023-02-06 15:51:31 -0500

    additional details display after selecting bug

--- frontend/src/Models/bugModel.js
+++ frontend/src/Models/bugModel.js
@@ -4,7 +4,7 @@ function bug(bug) {
     if(bug != undefined) {
         this._id = bug._id;
         this.name = bug.name; 
-        this.details = bug.detals;
+        this.details = bug.details;
         this.steps = bug.steps;
         this.version = bug.version;
         this.priority = bug.priority;
--- frontend/src/Views/Pages/viewBugs.jsx
+++ frontend/src/Views/Pages/viewBugs.jsx
@@ -1,9 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getBugs } from '../../Controllers/Redux/bugSlice';
-import BugCard from '../Bug Card/bugCard';
+import BugCard from '../Components/Bug Card/bugCard';
+import BugView from "../Components/Bug View/bugView";
 
 export default () => {
+
+    const [DISPLAY_BUG, SET_DISPLAY_BUG] = useState({
+        name: "",
+        isDisplayed: false
+    }); 
 
     const dispatch = useDispatch();
     const { bugs } = useSelector(state => state);
@@ -12,11 +18,19 @@ export default () => {
         dispatch(getBugs());
     }, [bugs.length < 1])
 
+    function BugClicked(name) {
+        SET_DISPLAY_BUG({
+            isDisplayed: !DISPLAY_BUG.isDisplayed,
+            name: name
+        })
+    }
+
     return (
         <div className="page-container">
             {bugs.map((bug, key) => (
-                <BugCard key={key} bug={bug} />
+                <BugCard key={key} bug={bug} clicked={BugClicked} />
             ))}
+            {DISPLAY_BUG.isDisplayed && <BugView bug={bugs.filter((bug) => bug.name == DISPLAY_BUG.name)[0]} />}
         </div>
     )
 }
