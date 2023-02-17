fbe9320548d9c3c271415e271313558c7ac981db
Tyler <Tyl3r.s@outlook.com>
2023-02-17 07:44:15 -0500

    dashboard renders total count and severity color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Login from './Views/Login/login.jsx';
+import Login from './Views/Pages/Login/login.jsx';
 import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './Views/Sidebar/sidebar.jsx';
-import dashboard from './Views/Pages/Dashboard/dashboard.jsx';
+import Dashboard from './Views/Pages/Dashboard/dashboard.jsx';
 import ViewBugPage from './Views/Pages/viewBugs';
 import CreateBug from './Views/Components/Bug Create/bugForm';
-import Dashboard from './Views/Pages/Dashboard/dashboard.jsx';
 
 function App() {
   const { auth } = useSelector(state => state)
--- frontend/src/Views/Components/Dashboard/card.jsx
+++ frontend/src/Views/Components/Dashboard/card.jsx
@@ -3,12 +3,12 @@ import './card.css';
 import Priority from '../../../Controllers/priorityController';
 
 export default (props) => {
-    const {level, color} = Priority(props.Priority)
+    const { level, color } = Priority(props.priority)
 
     return(
-        <div className="dashboard-card" style={{color: color}}>
-            <h2>Total: {level}</h2>
-            <p>{props.count}</p>
+        <div className="dashboard-card" onClick={props.clicked} style={{color:color}}>
+            <h2>Severity: {level}</h2>
+            <p>total: {props.count}</p>
         </div>
     )
 }
